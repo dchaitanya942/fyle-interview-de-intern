4bca8b777cccf4b2304c68f5f1779e9493836037
dchaitanya942 <dchaitanya942@gmail.com>
2021-08-31 01:43:45 +0530

    Completed extract_amount function

--- extract.py
+++ extract.py
@@ -1,5 +1,7 @@
 # Your imports go here
+import re
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -14,8 +16,22 @@ logger = logging.getLogger(__name__)
 
 '''
 def extract_amount(dirpath: str) -> float:
-
     logger.info('extract_amount called for dir %s', dirpath)
-    # your logic goes here
-
-    return 0.0
+    with open(dirpath + "/ocr.json","r",encoding='utf-8') as file:
+        data = file.read()
+        js = json.loads(data)
+    blocks = js['Blocks']
+    names = {"total","total:","theater and dance","order total","paid","payment","credit","debit"}
+    for x,_ in enumerate(blocks):
+        try:
+            if blocks[x]['Text'].lower() in names:
+                a = 1
+                while a<=2:
+                    if any(i.isdigit() for i in blocks[x+a]['Text']) and blocks[x+a]['Text'].find('/')==-1 and blocks[x+a]['Text']!="1" and blocks[x+a]['Text'].find('-')==-1 :
+                        amount = blocks[x+a]['Text']
+                        return float(re.sub("[^0123456789.]","",amount))
+                    else:
+                        a+=1
+        except KeyError:
+            pass
+    return float(re.sub("[^0123456789.]","",amount))
